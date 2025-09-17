72857a602967fe9335cfec57357725f84f5500ce
axelferreira100 <GUSTAVO.FERREIRA@correo.ucu.edu.uy>
2025-09-16 21:38:31 -0300

    Finished the challenge, only applied a for with an array of length determined by the variable length.

--- src/Library/Program.cs
+++ src/Library/Program.cs
@@ -4,6 +4,13 @@
     {
         public static int[] ArrayOfMultiples(int num, int length)
         {
+            int[] multiples = new int[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                multiples[i] = (num * (i + 1));
+            }
+            return multiples;
         }
     }
 }
